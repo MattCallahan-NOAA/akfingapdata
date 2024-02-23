224bd91e70a3d0b9513207e29c679dc148bfa4f4
MattCallahan-NOAA <matt.callahan@noaa.gov>
2024-02-23 11:57:09 -0900

    function updates
    Updated functions with warning and error messages if token or parameters are missing and included timing

--- R/get_gap_cruise.R
+++ R/get_gap_cruise.R
@@ -1,27 +1,35 @@
 #' get_gap_cruise
-#' @description This function pulls data from the akfin_agecomp table in the gap_products schema on the AKFIN database.
-#' This table is a copy of the biomass table produced by the groundfish assessment program (GAP)
-#' These data are docuented here: https://github.com/afsc-gap-products/gap_products
-#' get_gap_species() and get_gap_area() are related functions to look up species codes and area ids if necessary.
+#' @description This function pulls data from the akfin_cruise table in the gap_products schema on the AKFIN database.
 
-#' @param survey_definition_id character that designates regional survey. EBS-98, NBS-143, EBS slope-78, GOA-47, AI-52. Default is 47.
-#' @param startyear first year in the time series, default 1990.
-#' @param endyear last year in the time series, default to latest year,
 
-get_gap_cruise<-function(survey_definition_id=98, start_year=1990, end_year=3000) {
+get_gap_cruise<-function() {
+  start_time <- Sys.time()
 
-  # paste(... collapse=",") puts commas between vector elements
-  survey_definition_id<-paste(survey_definition_id, collapse = ",")
-  query <- list(survey_definition_id=survey_definition_id, start_year=start_year, end_year=end_year)
+  if(!exists("token")) {
+    message("Please create a valid Oauth token, e.g. token <- create_token('your_token.txt')")
+  }
+
   url <- "https://apex.psmfc.org/akfin/data_marts/gap_products/gap_cruise?"
 
-  httr::content(
-    httr::GET(url=url, #query=query,
-              add_headers(Authorization = token)),
-    type = "application/json") %>%
-    # convert to data frame
-    dplyr::bind_rows() %>%
-    dplyr::rename_with(tolower)
+  response <- httr::GET(url=url,
+                        httr::add_headers(Authorization = token))
+
+  if(httr::http_error(response) ) {
+    stop(paste0("status code ",
+                response$status_code,
+                ". For a 401 error you may need to supply a valid Oauth token, e.g. token <- create_token('your_token.txt')"))
+  }
+
+  data <- jsonlite::fromJSON(
+    httr::content(response, type = "text", encoding = "UTF-8")) |>
+    dplyr::bind_rows()
+
+  end_time <- Sys.time()
+  message(paste("Time Elapsed:", round(end_time - start_time, 2),
+                units(end_time - start_time)))
+  return(data)
+
 }
+
 
 
--- R/get_gap_sizecomp.R
+++ R/get_gap_sizecomp.R
@@ -11,20 +11,47 @@
 #' @param end_year last year in the time series, default to latest year,
 
 get_gap_sizecomp<-function(survey_definition_id=98, area_id=1, species_code=21740, start_year=1990, end_year=3000) {
+  start_time <- Sys.time()
 
-  # paste(... collapse=",") puts commas between vector elements
-  area_id <- paste(area_id, collapse = ",")
-  species_code <- paste(species_code, collapse = ",")
-  survey_definition_id<-paste(survey_definition_id, collapse = ",")
+  if(!exists("token")) {
+    message("Please create a valid Oauth token, e.g. token <- create_token('your_token.txt')")
+  }
+
   query <- list(survey_definition_id=survey_definition_id, area_id=area_id, species_code=species_code, start_year=start_year, end_year=end_year)
   url <- "https://apex.psmfc.org/akfin/data_marts/gap_products/gap_sizecomp?"
 
-  httr::content(
-    httr::GET(url=url, query=query,
-              add_headers(Authorization = token)),
-    type = "application/json") %>%
-    # convert to data frame
-    dplyr::bind_rows() %>%
-    dplyr::rename_with(tolower)
-}
+  response <- httr::GET(url=url, query=query,
+                        httr::add_headers(Authorization = token))
 
+  if(httr::http_error(response) ) {
+    stop(paste0("status code ",
+                response$status_code,
+                ". For a 401 error you may need to supply a valid Oauth token, e.g. token <- create_token('your_token.txt')"))
+  }
+
+  data <- jsonlite::fromJSON(
+    httr::content(response, type = "text", encoding = "UTF-8")) |>
+    dplyr::bind_rows()
+
+  if(missing(survey_definition_id)) {
+    message("No survey_definition_id provided, defaulting to 98 (Bering Sea)")
+  }
+  if(missing(species_code)) {
+    message("No species_code provided, defaulting to 21740 (Pollock)")
+  }
+  if(missing(area_id)) {
+    message("No area_id provided, defaulting to 99900 (EBS Standard Plus NW Region: All Strata)")
+  }
+  if(missing(start_year)) {
+    message("No start_year provided, defaulting to 1990")
+  }
+  if(missing(end_year)) {
+    message("No end_year provided, defaulting to latest year")
+  }
+
+  end_time <- Sys.time()
+  message(paste("Time Elapsed:", round(end_time - start_time, 2),
+                units(end_time - start_time)))
+  return(data)
+
+}
