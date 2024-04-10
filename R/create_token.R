940864d8e53b9ca9a69890ba459537936af843f5
MattCallahan-NOAA <matt.callahan@noaa.gov>
2024-04-10 15:24:40 -0800

    Added keying option for create_token()

--- R/create_token.R
+++ R/create_token.R
@@ -1,10 +1,19 @@
 #' create_token
 #' @description This function creates a token necessary for web service authentication
-#' token<- create_token("Callahan_token.txt)
+#' The output must be defined as token for the other functions to work
+#' This function accepts keyring and text file input
+#' token<- create_token("akfin_secret") # keyring service storing secret string
+#' token<- create_token("Callahan_token.txt") # text file storing your secret string
 
-create_token<-function(file) {
-  # Secret string text file needs to be in your working R directory
-  secret <- jsonlite::base64_enc( readChar(file,nchars=1e6) )
+create_token<-function(x) {
+#check key_list for input
+  if (x %in% keyring::key_list()$service) {
+    secret <- jsonlite::base64_enc( keyring::key_get(x))
+  }
+# If not found, assume it's a file and load it.
+  else {
+    secret <- jsonlite::base64_enc(readChar(x,nchars=1e6) )
+  }
 
   # Get token from API
   req <- httr::POST("https://apex.psmfc.org/akfin/data_marts/oauth/token",
@@ -19,3 +28,4 @@ create_token<-function(file) {
   paste("Bearer", httr::content(req)$access_token)
 
 }
+
