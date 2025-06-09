2bbf00ae8d73d892586df76c3df8df4b045eb0cd
MattCallahan-NOAA <matt.callahan@noaa.gov>
2025-06-09 08:58:38 -0800

    function that joins tables

--- R/create_token.R
+++ R/create_token.R
@@ -1,10 +1,27 @@
 #' create_token
 #' @description This function creates a token necessary for web service authentication
-#' token<- create_token("Callahan_token.txt)
+#' The output must be defined as token for the other functions to work
+#' Define your authentication string
+#' x<-"12345..:abcd.."
+#' token <- create_token(x)
+#' Otherwise, this function accepts keyring and text file input
+#' token<- create_token("akfin_secret") # keyring service storing secret string
+#' token<- create_token("Callahan_token.txt") # text file storing your secret string
 
-create_token<-function(file) {
-  # Secret string text file needs to be in your working R directory
-  secret <- jsonlite::base64_enc( readChar(file,nchars=1e6) )
+create_token<-function(x) {
+
+  # Check if x exists in the global environment
+  if (!is.character(x)) {
+    secret <- jsonlite::base64_enc(x)
+
+  #check key_list for input
+  } else if (x %in% keyring::key_list()$service) {
+    secret <- jsonlite::base64_enc( keyring::key_get(x))
+  }
+# If not found, assume it's a file and load it.
+  else {
+    secret <- jsonlite::base64_enc(readChar(x,nchars=1e6) )
+  }
 
   # Get token from API
   req <- httr::POST("https://apex.psmfc.org/akfin/data_marts/oauth/token",
